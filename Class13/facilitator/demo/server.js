199bb2debf9dc79552f5fecd43d19dc04ed870c1
Eng-Ahmad-Almohammad <almohammedahmed2@gmail.com>
2022-01-25 15:42:31 +0200

    Fix the demo for the new API

--- Class13/facilitator/demo/server.js
+++ Class13/facilitator/demo/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cors());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const client = new pg.Client(process.env.DATABASE_URL);
 
@@ -23,25 +23,28 @@ var jsonParser = bodyParser.json();
 
 app.get('/', helloWorldHandler);
 
-app.get('/memes', memesHandler);
+app.get('/facts', AnimalFactsHandler);
 
-app.get('/memesPage/', memesPageHandler)
+app.get('/factsNum', factsNumHandler);
 
-app.post('/addFavMeme' ,jsonParser, addFavMemeHandler)
+app.post('/addFavFact' ,jsonParser, addFavFactHandler);
 
-app.get('/favmeme', getFavMemesHandler);
+app.get('/favFact', getFavFactsHandler);
 
 
 app.use('*', notFoundHandler);
 
 app.use(errorHandler)
 
-function Meme(id, name, image, tag, topText){
+function AnimalFact(id, name, image_link, animal_type, length_min, length_max, habitat, diet){
     this.id = id;
     this.name = name;
-    this.image = image;
-    this.tag = tag;
-    this.topText = topText;
+    this.image_link = image_link;
+    this.animal_type = animal_type;
+    this.length_min = length_min;
+    this.length_max = length_max;
+    this.habitat = habitat;
+    this.diet = diet;
 }
 
 
@@ -50,36 +53,36 @@ function helloWorldHandler(req , res){
     return res.status(200).send("Hello World");
 }
 
-function memesHandler(req , res){
-    let memes = []
+function AnimalFactsHandler(req , res){
+    let facts = []
     
-    axios.get('http://alpha-meme-maker.herokuapp.com/')
+    axios.get('https://zoo-animal-api.herokuapp.com/animals/rand/10')
     .then(result => {
-        result.data.data.map(meme => {
-            let oneMeme = new Meme(meme.ID, meme.name, meme.image, meme.tags,meme.topText)
-            console.log(oneMeme);
-            memes.push(oneMeme);
+        result.data.map(fact => {
+            let oneFact = new AnimalFact(fact.id, fact.name, fact.image_link, fact.animal_type, fact.length_min, fact.length_max, fact.habitat, fact.diet);
+            console.log(oneFact);
+            facts.push(oneFact);
         })
-        return res.status(200).json(memes);
+        return res.status(200).json(facts);
     })
     .catch(error => {
         errorHandler(error, req,res);
     })
 }
 
-function memesPageHandler(req, res){
+function factsNumHandler(req, res){
     try{
-        console.log(req.query.page);
-        let memes = []
-        let pageNum = req.query.page;
-        axios.get(`http://alpha-meme-maker.herokuapp.com/${pageNum}/`)
+        console.log(req.query.number);
+        let facts = []
+        let factNum = req.query.number;
+        axios.get(`https://zoo-animal-api.herokuapp.com/animals/rand/${factNum}/`)
         .then(result => {
-            result.data.data.map(meme => {
-                let oneMeme = new Meme(meme.ID, meme.name, meme.image, meme.tags,meme.topText)
-                console.log(oneMeme);
-                memes.push(oneMeme);
+            result.data.map(fact => {
+                let oneFact = new AnimalFact(fact.id, fact.name, fact.image_link, fact.animal_type, fact.length_min, fact.length_max, fact.habitat, fact.diet);
+                console.log(oneFact);
+                facts.push(oneFact);
             })
-            return res.status(200).json(memes);
+            return res.status(200).json(facts);
         })
         .catch(error => {
             errorHandler(error, req,res);
@@ -92,24 +95,24 @@ function memesPageHandler(req, res){
     }
 }
 
-function addFavMemeHandler(req, res){
-    const meme = req.body;
-    console.log(meme);
-    const sql = `INSERT INTO favmeme(memeName, memeImage, tags, topText, comment) VALUES($1, $2, $3, $4, $5) RETURNING *;`
+function addFavFactHandler(req, res){
+    const fact = req.body;
+    console.log(fact);
+    const sql = `INSERT INTO favanimalfact(name, factImage, animalType, minLength, maxLength, habitat, diet, comment) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;`
 
-    const values = [meme.name,meme.image, meme.tags, meme.topText, meme.comment];
-
+    const values = [fact.name, fact.image_link, fact.animal_type, fact.length_min, fact.length_max, fact.habitat, fact.diet, fact.comment];
     client.query(sql,values).then((data) => {
         res.status(201).json(data.rows);
     })
     .catch(error => {
+        console.log(error);
         errorHandler(error, req,res);
     });
 };
 
-function getFavMemesHandler(req, res){
+function getFavFactsHandler(req, res){
 
-    const sql = `SELECT * FROM favmeme`;
+    const sql = `SELECT * FROM favanimalfact`;
 
     client.query(sql).then(data => {
         return res.status(200).json(data.rows);
@@ -117,7 +120,7 @@ function getFavMemesHandler(req, res){
     .catch(error => {
         errorHandler(error, req,res);
     });
-}
+};
 
 function notFoundHandler(request,response) { 
     response.status(404).send('huh????');
