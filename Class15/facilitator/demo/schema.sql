dab27e0ec4d3b78434eea69bfb51be2db9de3bcf
Eng-Ahmad-Almohammad <almohammedahmed2@gmail.com>
2022-01-26 19:08:19 +0200

    Use new API

--- Class15/facilitator/demo/schema.sql
+++ Class15/facilitator/demo/schema.sql
@@ -1,10 +1,13 @@
-DROP TABLE IF EXISTS favmeme;
+DROP TABLE IF EXISTS favRecipes;
 
-CREATE TABLE IF NOT EXISTS favmeme (
+CREATE TABLE IF NOT EXISTS favRecipes (
     id SERIAL PRIMARY KEY,
-    memeName VARCHAR(255),
-    memeImage VARCHAR(255),
-    tags VARCHAR(255),
-    topText VARCHAR(255),
+    title VARCHAR(255),
+    readyInMinutes INTEGER,
+    summary VARCHAR(10000),
+    vegetarian BOOLEAN,
+    instructions VARCHAR(10000),
+    sourceUrl VARCHAR(255),
+    image VARCHAR(255),
     comment VARCHAR(255)
 );
--- Class15/facilitator/demo/server.js
+++ Class15/facilitator/demo/server.js
@@ -16,9 +16,11 @@ app.use(cors());
 
 const PORT = process.env.PORT;
 
+const apiKey = process.env.APIKEY;
+
 const client = new pg.Client({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+    // ssl: { rejectUnauthorized: false }
 })
 
 // const client = new pg.Client(process.env.DATABASE_URL);
@@ -29,30 +31,33 @@ var jsonParser = bodyParser.json();
 
 app.get('/', helloWorldHandler);
 
-app.get('/memes', memesHandler);
+app.get('/recipes', recipesHandler);
 
-app.get('/memesPage/', memesPageHandler)
+app.get('/searchRecipes', searchRecipesHandler);
 
-app.post('/addFavMeme' ,jsonParser, addFavMemeHandler)
+app.post('/addFavRecipe' ,jsonParser, addFavRecipeHandler);
 
-app.get('/favmeme', getFavMemesHandler);
+app.get('/favRecipes', getFavRecipesHandler);
 
-app.get('/favmeme/:id', getFavMemeHandler);
+app.get('/favRecipe/:id', getFavRecipeHandler);
 
-app.put('/updateFavMeme/:id', jsonParser,updateFavMemeHandler )
+app.put('/updateFavRecipe/:id', jsonParser,updateFavRecipeHandler);
 
-app.delete('/deleteFavMeme/:id', deleteFavMemeHandler);
+app.delete('/deleteFavRecipe/:id', deleteFavRecipeHandler);
 
 app.use('*', notFoundHandler);
 
 app.use(errorHandler)
 
-function Meme(id, name, image, tag, topText){
+function Recipe(id, title, readyInMinutes, summary, vegetarian, instructions, sourceUrl, image){
     this.id = id;
-    this.name = name;
+    this.title = title;
+    this.readyInMinutes = readyInMinutes;
+    this.summary = summary;
+    this.vegetarian = vegetarian;
+    this.instructions = instructions;
+    this.sourceUrl = sourceUrl;
     this.image = image;
-    this.tag = tag;
-    this.topText = topText;
 }
 
 
@@ -61,38 +66,38 @@ function helloWorldHandler(req , res){
     return res.status(200).send("Hello World");
 }
 
-function memesHandler(req , res){
-    let memes = []
-    
-    axios.get('http://alpha-meme-maker.herokuapp.com/')
+function recipesHandler(req , res){
+    let recipes = []
+    let numberOfReturnedData = 10; // 1 ==> 100
+    axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=${numberOfReturnedData}`)
     .then(result => {
-        result.data.data.map(meme => {
-            let oneMeme = new Meme(meme.ID, meme.name, meme.image, meme.tags,meme.topText)
-            console.log(oneMeme);
-            memes.push(oneMeme);
+        result.data.recipes.map(recipe => {
+            let oneRecipe = new Recipe(recipe.id, recipe.title || '', recipe.readyInMinutes || '', recipe.summary || '', recipe.vegetarian , recipe.instructions || '', recipe.sourceUrl || '', recipe.image || '');
+            recipes.push(oneRecipe);
         })
-        return res.status(200).json(memes);
+        return res.status(200).json(recipes);
     })
     .catch(error => {
+        
         errorHandler(error, req,res);
     })
 }
 
-function memesPageHandler(req, res){
+function searchRecipesHandler(req, res){
     try{
-        console.log(req.query.page);
-        let memes = []
-        let pageNum = req.query.page;
-        axios.get(`http://alpha-meme-maker.herokuapp.com/${pageNum}/`)
+        console.log(req.query.search);
+        let recipes = []
+        let query = req.query.search;
+        axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}/`)
         .then(result => {
-            result.data.data.map(meme => {
-                let oneMeme = new Meme(meme.ID, meme.name, meme.image, meme.tags,meme.topText)
-                console.log(oneMeme);
-                memes.push(oneMeme);
+            result.data.results.map(recipe => {
+                let oneRecipe = new Recipe(recipe.id, recipe.title || '', recipe.readyInMinutes || '', recipe.summary || '', recipe.vegetarian , recipe.instructions || '', recipe.sourceUrl || '', recipe.image || '');
+                recipes.push(oneRecipe);
             })
-            return res.status(200).json(memes);
+            return res.status(200).json(recipes);
         })
         .catch(error => {
+            console.log(error);
             errorHandler(error, req,res);
         });
 
@@ -103,37 +108,24 @@ function memesPageHandler(req, res){
     }
 }
 
-function addFavMemeHandler(req, res){
-    const meme = req.body;
-    console.log(meme);
-    const sql = `INSERT INTO favmeme(memeName, memeImage, tags, topText, comment) VALUES($1, $2, $3, $4, $5) RETURNING *;`
+function addFavRecipeHandler(req, res){
+    const recipe = req.body;
+    console.log(recipe);
+    const sql = `INSERT INTO favRecipes(title, readyInMinutes, summary, vegetarian, instructions, sourceUrl, image, comment) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;`
 
-    const values = [meme.name,meme.image, meme.tags, meme.topText, meme.comment];
-
+    const values = [recipe.title, recipe.readyInMinutes, recipe.summary, recipe.vegetarian, recipe.instructions, recipe.sourceUrl, recipe.image, recipe.comment];
     client.query(sql,values).then((data) => {
-        res.status(200).json(data.rows);
+        res.status(201).json(data.rows);
     })
     .catch(error => {
+        console.log(error);
         errorHandler(error, req,res);
     });
 };
 
-function getFavMemesHandler(req, res){
+function getFavRecipesHandler(req, res){
 
-    const sql = `SELECT * FROM favmeme`;
-
-    client.query(sql).then(data => {
-        return res.status(200).json(data.rows);
-    })
-    .catch(error => {
-        errorHandler(error, req,res);
-    });
-}
-
-function getFavMemeHandler(req,res){
-    const id = req.params.id;
-
-    const sql = `SELECT * FROM favmeme WHERE id = ${id}`;
+    const sql = `SELECT * FROM favRecipes`;
 
     client.query(sql).then(data => {
         return res.status(200).json(data.rows);
@@ -143,27 +135,41 @@ function getFavMemeHandler(req,res){
     });
 };
 
-function updateFavMemeHandler(req, res){
+function getFavRecipeHandler(req,res){
     const id = req.params.id;
-    const meme = req.body;
 
-    const sql = `UPDATE favmeme SET memeName=$1, memeImage=$2, tags=$3, topText=$4, comment=$5 WHERE id=${id} RETURNING *;`;
-    const values = [meme.name, meme.image, meme.tags, meme.topText, meme.comment];
+    const sql = `SELECT * FROM favRecipes WHERE id = ${id}`;
+
+    client.query(sql).then(data => {
+        return res.status(200).json(data.rows);
+    })
+    .catch(error => {
+        errorHandler(error, req,res);
+    });
+};
+
+function updateFavRecipeHandler(req, res){
+    const id = req.params.id;
+    const recipe = req.body;
+
+    const sql = `UPDATE favRecipes SET title=$1, readyInMinutes=$2, summary=$3, vegetarian=$4, instructions=$5, sourceUrl=$6, image=$7, comment=$8 WHERE id=${id} RETURNING *;`;
+    const values = [recipe.title, recipe.readyInMinutes, recipe.summary, recipe.vegetarian, recipe.instructions, recipe.sourceUrl, recipe.image, recipe.comment];
 
     client.query(sql, values).then(data => {
         return res.status(200).json(data.rows);
         // or you can send 204 status with no content
         // return res.status(200).json(data.rows);
     }).catch( err => {
+        console.log(err);
         errorHandler(err,req,res);
     });
 
 };
 
-function deleteFavMemeHandler(req , res){
+function deleteFavRecipeHandler(req , res){
     const id = req.params.id;
 
-    const sql = `DELETE FROM favmeme WHERE id=${id};`;
+    const sql = `DELETE FROM favRecipes WHERE id=${id};`;
 
     client.query(sql).then(() => {
         return res.status(204).json({});
