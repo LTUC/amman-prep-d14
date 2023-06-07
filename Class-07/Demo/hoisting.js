bbfa92260e8e1926bffb59447cb9c7f586f720fc
Maisabdalrazeq <maisabdalrazq@gmail.com>
2023-06-07 15:35:10 +0300

    feat : adding files and demo for class07

---
+++ Class-07/Demo/hoisting.js
@@ -0,0 +1,31 @@
+// 3 loops
+// for loop , while loop , do while 
+// function hello(parm1 ,param2){
+//    any code you want
+// }
+// hello();
+greeting();
+console.log(x);
+greeting2();
+// console.log(favColor);
+// console.log(i); 
+function greeting() {
+    console.log("hello Java students");
+}
+greeting();
+
+var greeting2 = function () {
+    console.log("hiii");
+}
+
+greeting2();
+
+var x = 5;
+console.log(x);
+
+let i;
+i = "mais";
+console.log(i);
+
+const favColor = ["pink", "white"];
+console.log(favColor);
